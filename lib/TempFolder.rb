8ce694560c8b72f39a09fa6ea9ebb7eebd455cf0
Camilo Sanchez <camilin87@gmail.com>
2017-03-05 09:49:09 -0500

    create a temp folder helper

---
+++ lib/TempFolder.rb
@@ -0,0 +1,16 @@
+require 'fileutils'
+require 'tmpdir'
+
+class TempFolder
+    def path
+        @temp_path
+    end
+
+    def initialize
+        @temp_path = Dir.mktmpdir
+    end
+
+    def destroy
+        FileUtils.rm_rf @temp_path
+    end
+end
