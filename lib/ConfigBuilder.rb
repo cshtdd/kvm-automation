52ab4fb456e59bd3b4df83a7813cca8ffa765e64
Camilo Sanchez <camilin87@gmail.com>
2017-03-05 08:22:31 -0500

    rename the rsa key public parameter

--- lib/ConfigBuilder.rb
+++ lib/ConfigBuilder.rb
@@ -1,9 +1,9 @@
 class ConfigBuilder
-    def self.generate_cloud_config(public_rsa_key, vm_name)
+    def self.generate_cloud_config(rsa_public_key, vm_name)
         %{
 hostname: "#{vm_name}"
 ssh-authorized-keys:
-    - #{public_rsa_key}
+    - #{rsa_public_key}
 }
     end
 end
