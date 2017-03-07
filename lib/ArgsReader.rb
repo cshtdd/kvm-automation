d17d43f67bb3d6b5583824b40886ba3313c4014f
Camilo Sanchez <camilin87@gmail.com>
2017-03-06 22:26:21 -0500

    define the things that can be configured

---
+++ lib/ArgsReader.rb
@@ -0,0 +1,37 @@
+class ArgsReader
+    def initialize(input=ARGV)
+        @input_arr = input
+    end
+
+    def storage_folder
+        @storage_folder || File.expand_path("~/vms")
+    end
+
+    def public_key_filename
+        @public_key_filename || File.expand_path("~/.ssh/id_rsa.pub")
+    end
+
+    def base_image_filename
+        @base_image_filename || ""
+    end
+
+    def vm_name
+        @vm_name || "vm01"
+    end
+
+    def mac_address
+        @mac_address || "54:36:E2:84:5A:C0"
+    end
+
+    def bridge_adapter
+        @bridge_adapter || "br0"
+    end
+
+    def ram_mb
+        @ram_mb || "1024"
+    end
+
+    def cpu_count
+        @cpu_count || "1"
+    end
+end
---
+++ spec/ArgsReader_spec.rb
@@ -0,0 +1,16 @@
+require "ArgsReader"
+
+describe "ArgsReader" do
+    it "returns default values when nothing is specified" do
+        r = ArgsReader.new()
+
+        expect(r.storage_folder).to eq(File.expand_path("~/vms"))
+        expect(r.public_key_filename).to eq(File.expand_path("~/.ssh/id_rsa.pub"))
+        expect(r.base_image_filename).to eq("")
+        expect(r.vm_name).to eq("vm01")
+        expect(r.mac_address).to eq("54:36:E2:84:5A:C0")
+        expect(r.bridge_adapter).to eq("br0")
+        expect(r.ram_mb).to eq("1024")
+        expect(r.cpu_count).to eq("1")
+    end
+end
