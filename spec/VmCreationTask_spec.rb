ba1e604be6394ee7f7a8d898c6e10ef53ed9f14e
Camilo Sanchez <camilin87@gmail.com>
2017-03-11 08:48:47 -0500

    refactor the tests a lil bit

--- spec/VmCreationTask_spec.rb
+++ spec/VmCreationTask_spec.rb
@@ -3,24 +3,22 @@ require "VmManager"
 require "TaskConfig"
 
 describe VmCreationTask, "run" do
-    it "generates the vm config drive" do
-        vm_manager = instance_double("VmManager").as_null_object
-        expect(vm_manager).to receive(:generate_vm_config_drive).with("my key")
+    before do
+        @vm_manager = instance_double("VmManager").as_null_object
 
+        @factory_stub = class_double("VmManagerFactory")
+            .as_stubbed_const(:transfer_nested_constants => true)
+    end
+
+    it "generates the vm config drive" do
         config = instance_double("TaskConfig",
             :public_key_filename => "my key",
             :vm_name => "test vm",
             :storage_folder => "test storage"
         )
+        expect(@vm_manager).to receive(:generate_vm_config_drive).with("my key")
+        expect(@factory_stub).to receive(:create).with(config).and_return(@vm_manager)
 
-        factory_stub = class_double("VmManagerFactory")
-            .as_stubbed_const(:transfer_nested_constants => true)
-        expect(factory_stub).to receive(:create).with(config).and_return(vm_manager)
-
-        task = VmCreationTask.new(config: config)
-
-        task.run
+        VmCreationTask.new(config: config).run
     end
-
-
 end
