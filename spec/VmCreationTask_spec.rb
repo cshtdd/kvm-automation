fda95c568b75cb1b3150957e4a079ec8528112b4
Camilo Sanchez <camilin87@gmail.com>
2017-03-14 20:59:22 -0400

    cleanup the tests even more

--- spec/VmCreationTask_spec.rb
+++ spec/VmCreationTask_spec.rb
@@ -8,24 +8,25 @@ describe VmCreationTask, "run" do
 
         @factory_stub = class_double("VmManagerFactory")
             .as_stubbed_const(:transfer_nested_constants => true)
-
-        @config = instance_double("TaskConfig",
-            :public_key_filename => "my key",
-            :vm_name => "test vm",
-            :storage_folder => "test storage",
-            :base_image_filename => "seeded_base_image.img"
-        )
-        expect(@factory_stub).to receive(:create).with(@config).and_return(@vm_manager)
     end
 
     it "generates the vm config drive" do
+        @config = instance_double("TaskConfig",
+            :public_key_filename => "my key"
+        ).as_null_object
+        expect(@factory_stub).to receive(:create).with(@config).and_return(@vm_manager)
+
         expect(@vm_manager).to receive(:generate_vm_config_drive).with("my key")
-        allow(@vm_manager).to receive(:create_vm_hdd)
 
         VmCreationTask.new(config: @config).run
     end
 
     it "generates the hdd" do
+        @config = instance_double("TaskConfig",
+            :base_image_filename => "seeded_base_image.img"
+        ).as_null_object
+        expect(@factory_stub).to receive(:create).with(@config).and_return(@vm_manager)
+
         expect(@vm_manager).to receive(:create_vm_hdd).with("seeded_base_image.img")
 
         VmCreationTask.new(config: @config).run
