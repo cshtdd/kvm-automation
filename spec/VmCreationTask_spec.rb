0e8cdcb5851f75c52dd01de7e90ed5fc3573ca77
Camilo Sanchez <camilin87@gmail.com>
2017-03-14 21:04:42 -0400

    refactor the tests to remove some duplication

--- spec/VmCreationTask_spec.rb
+++ spec/VmCreationTask_spec.rb
@@ -10,34 +10,36 @@ describe VmCreationTask, "run" do
             .as_stubbed_const(:transfer_nested_constants => true)
     end
 
+    def run_task
+        expect(@factory_stub).to receive(:create).with(@config).and_return(@vm_manager)
+        VmCreationTask.new(config: @config).run
+    end
+
     it "removes existing vms with that name" do
         @config = instance_double("TaskConfig").as_null_object
-        expect(@factory_stub).to receive(:create).with(@config).and_return(@vm_manager)
 
         expect(@vm_manager).to receive(:destroy_existing_vm)
 
-        VmCreationTask.new(config: @config).run
+        run_task
     end
 
     it "generates the vm config drive" do
         @config = instance_double("TaskConfig",
             :public_key_filename => "my key"
         ).as_null_object
-        expect(@factory_stub).to receive(:create).with(@config).and_return(@vm_manager)
 
         expect(@vm_manager).to receive(:generate_vm_config_drive).with("my key")
 
-        VmCreationTask.new(config: @config).run
+        run_task
     end
 
     it "generates the hdd" do
         @config = instance_double("TaskConfig",
             :base_image_filename => "seeded_base_image.img"
         ).as_null_object
-        expect(@factory_stub).to receive(:create).with(@config).and_return(@vm_manager)
 
         expect(@vm_manager).to receive(:create_vm_hdd).with("seeded_base_image.img")
 
-        VmCreationTask.new(config: @config).run
+        run_task
     end
 end
