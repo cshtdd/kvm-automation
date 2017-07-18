93d9dd09ca2a06ad9cc1d9f51ac48c55f6bf37c4
Camilo Sanchez <camilin87@gmail.com>
2017-07-17 20:59:22 -0400

    restores the ubuntu vm out of the base image

--- lib/UbuntuVmRestoreTask.rb
+++ lib/UbuntuVmRestoreTask.rb
@@ -7,5 +7,15 @@ class UbuntuVmRestoreTask < VmTask
         vm_manager.create_vm_hdd_container_folder
 
         vm_manager.extract_vm_backup @config.base_image_filename
+
+        vm_manager.restore_ubuntu_vm(
+            @config.os_variant,
+            @config.mac_address,
+            @config.bridge_adapter,
+            @config.ram_mb,
+            @config.cpu_count,
+            @config.vnc_port,
+            @config.vnc_ip
+        )
     end
 end
--- spec/UbuntuVmRestoreTask_spec.rb
+++ spec/UbuntuVmRestoreTask_spec.rb
@@ -28,12 +28,30 @@ describe UbuntuVmRestoreTask, "run" do
         run_task
     end
 
-    it "extracts the compressed backup" do 
+    it "extracts the compressed backup" do
         @config = instance_double("TaskConfig",
             :base_image_filename => "backups/disk1.img.gz"
         ).as_null_object
 
         expect(@vm_manager).to receive(:extract_vm_backup).with("backups/disk1.img.gz")
+
+        run_task
+    end
+
+    it "restores the vm" do
+        @config = instance_double("TaskConfig",
+            :os_variant => "minix",
+            :mac_address => "my mac",
+            :bridge_adapter => "br-test",
+            :ram_mb => "12345",
+            :cpu_count => "10",
+            :vnc_port => "6800",
+            :vnc_ip => "127.0.0.1"
+        ).as_null_object
+
+        expect(@vm_manager).to receive(:restore_ubuntu_vm).with(
+            "minix", "my mac", "br-test", "12345", "10", "6800", "127.0.0.1"
+        )
 
         run_task
     end
