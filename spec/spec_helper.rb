3914265ff5b43f801266cfdd44e1779e4d168fdc
Camilo Sanchez <camilin87@gmail.com>
2017-07-19 22:12:45 -0400

    don't suppress std errors in the tests output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,15 +18,11 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
 
-  original_stderr = $stderr
   original_stdout = $stdout
   config.before(:all) do
-    # Redirect stderr and stdout
-    $stderr = File.open(File::NULL, "w")
     $stdout = File.open(File::NULL, "w")
   end
   config.after(:all) do
-    $stderr = original_stderr
     $stdout = original_stdout
   end
 
