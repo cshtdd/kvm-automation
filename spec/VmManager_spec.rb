17adfbe0efd5b360fb693b1380c289c0b1c54abf
Camilo Sanchez <camilin87@gmail.com>
2017-07-19 21:13:48 -0400

    remove the parenthesis around eq

--- spec/VmManager_spec.rb
+++ spec/VmManager_spec.rb
@@ -19,36 +19,37 @@ describe VmManager do
 
         expected_file_path = File.join(@tmp_root.path, "#{@vm_name}/openstack/latest/user_data")
 
-        expect(File.read(expected_file_path)).to eq("config contents")
+        expect(File.read(expected_file_path)).to eq "config contents"
     end
 
     it "retrieves the config folder path" do
         expected_config_folder_path = File.join(@tmp_root.path, "#{@vm_name}/")
 
-        expect(@m.config_folder).to eq(expected_config_folder_path)
+        expect(@m.config_folder).to eq expected_config_folder_path
     end
 
     it "determines where to store the hdd" do
         expected_hdd_filename = File.join(@tmp_root.path, "#{@vm_name}/#{@vm_name}.qcow2")
 
-        expect(@m.hdd_filename).to eq(expected_hdd_filename)
+        expect(@m.hdd_filename).to eq expected_hdd_filename
     end
 
     it "determines where to store the coreos image" do
         expect_coreos_image_filename = File.join(@tmp_root.path, "#{@vm_name}/coreos_production_qemu_image.img")
 
-        expect(@m.coreos_image_filename).to eq(expect_coreos_image_filename)
+        expect(@m.coreos_image_filename).to eq expect_coreos_image_filename
     end
 
     it "determines where to store the backup" do
         expected_backup_folder_path = File.join(@tmp_root.path, "#{@vm_name}/")
 
-        expect(@m.backup_folder).to eq(expected_backup_folder_path)
+        expect(@m.backup_folder).to eq expected_backup_folder_path
     end
 
     it "determines the snapshot folder" do
         expected_snapshot_folder_path = File.join(@tmp_root.path, "#{@vm_name}/19901201053223/")
 
-        expect(@m.snapshot_folder).to eq(expected_snapshot_folder_path)
+        expect(@m.snapshot_folder).to eq expected_snapshot_folder_path
+    end
     end
 end
